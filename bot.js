729859ae20d6611e6cc0d9216713511efc6ea10b
fleund <come.cothenet@hotmail.fr>
2017-05-15 16:33:51 +0200

    truc

--- bot.js
+++ bot.js
@@ -1,6 +1,7 @@
 const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
+const restClient = require('node-rest-client-promise').Client()
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
@@ -12,8 +13,17 @@ client.on('message', msg => {
   if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
 
   // If message is hello, post hello too
-  if (msg.content === 'hello') {
-    msg.channel.sendMessage('Hooray ! People are paying attention to me')
+  if (msg.substring(0, 4) === 'meteo') {
+    restClient.getPromise('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b05787eda8d8f7967925692ea52134d2')
+    // Il faudra récupérer un ID pour l'API, sur le site d'openweathermap
+      .catch((error) => {
+        throw error
+      })
+      .then((res) => {
+        console.log(res)
+        msg.channel.sendMessage(res.data.main.temp)
+        console.log(res.response.statusCode)
+      })
   } else if (msg.content === 'help') {
     msg.channel.sendMessage('Don\'t be sad, you still have a zoidberg')
   } else if (msg.content === 'how are you?') {
@@ -22,5 +32,4 @@ client.on('message', msg => {
     msg.channel.sendMessage('You’ll never guess where I’ve been!')
   }
 })
-
 client.login(config.token)
