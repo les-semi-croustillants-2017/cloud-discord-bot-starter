6f7129d619ded4078c632ab5b71edfda5b38bdfd
AlexandreCorre <alexandre.corre@isep.fr>
2017-05-15 19:01:56 +0200

    test

--- bot.js
+++ bot.js
@@ -2,8 +2,6 @@ const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
 
-import test from 'ava'
-
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`)
 })
@@ -12,18 +10,6 @@ client.on('message', msg => {
   // Check if the message has been posted in a channel where the bot operates
   // and that the author is not the bot itself
   if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
-
-  // If message is hello, post hello too
-  /*
-  if (msg.content === 'hello') {
-    msg.channel.sendMessage('Hello darkness my old friend...')
-  }
-  if (msg.content === 'bonjour') {
-    msg.channel.sendMessage('Moi pas parler baguette, sorry')
-  }
-  if (msg.content === 'hola') {
-    msg.channel.sendMessage('Hola tapas, nachos,tacos!!!')
-  }*/
   if (msg.content.substring(msg.content.lastIndexOf('!trad')) === '!trad') {
     msg.channel.sendMessage('Pour traduire taper !trad votre_texte key_de_la_langue')
   }
@@ -31,22 +17,11 @@ client.on('message', msg => {
   if (msg.content.lastIndexOf('!trad') !== -1) {
     var target
     var q
-    var key = 'AIzaSyDAT89ZOp4FiQocVj8AUho0zwSFkWzLjmM'
+    // var key = 'AIzaSyDAT89ZOp4FiQocVj8AUho0zwSFkWzLjmM'
     target = 'en'
     q = msg.content.substring(msg.content.lastIndexOf('!trad') + '!trad'.length, msg.content.length)
     msg.channel.sendMessage('Traduire ' + q + ' en ' + target)
     // GET https://www.googleapis.com/language/translate/v2?key=INSERT-YOUR-KEY&source=fr&target=en&q=Hello%20world
-    test('Example test', t => {
-      return client.getPromise('https://www.googleapis.com/language/translate/v2?key=INSERT-YOUR-KEY&source=fr&target=en&q=Hello%20world')
-        .catch((error) => {
-          t.fail()
-          throw error
-        })
-        .then((res) => {
-          console.log(res.response.statusCode)
-          t.is(res.response.statusCode, 200)
-        })
-    })
   }
 
   // msg.channel.sendMessage(msg.content.lastIndexOf('!trad'))
