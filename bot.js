a1c756c394938a61b9d0a911f09c1e05551c65f8
fleund <come.cothenet@hotmail.fr>
2017-05-22 13:55:10 +0200

    Travis et bot

--- bot.js
+++ bot.js
@@ -1,23 +1,23 @@
-const Discord = require('discord.js')
-const config = require('./config.js')
-const client = new Discord.Client()
-const restClient = require('node-rest-client-promise').Client
-// var city = ''
+  const Discord = require('discord.js')
+  const config = require('./config.js')
+  const client = new Discord.Client()
+  var restClient = require('node-rest-client-promise').Client
+  var city = ''
 
-client.on('ready', () => {
-  console.log(`Logged in as ${client.user.username}!`)
-})
+  client.on('ready', () => {
+    console.log(`Logged in as ${client.user.username}!`)
+  })
 
-client.on('message', msg => {
+  client.on('message', msg => {
   // Check if the message has been posted in a channel where the bot operates
   // and that the author is not the bot itself
-  if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
+    if (msg.channel.type !== 'dm' && (config.channel !== msg.channel.id || msg.author.id === client.user.id)) return
 
   // If message is hello, post hello too
-  if (msg.content.match('meteo*') !== null) {
-    // city = msg.content.substring(6, msg.content.length)
-    // msg.channel.sendMessage(city + ' ' + city.type)
-    restClient.getPromise('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=b05787eda8d8f7967925692ea52134d2')
+    if (msg.content.match('meteo*') !== null) {
+      city = msg.content.substring(6, msg.content.length)
+      msg.channel.sendMessage(city + ' ')
+      restClient.getPromise('http://api.openweathermap.org/data/2.5/weather?q=London&APPID=602b7069e6bd9de7d27ad28bfca04cc3')
       .catch((error) => {
         throw error
       })
@@ -27,12 +27,12 @@ client.on('message', msg => {
         console.log(res.response.statusCode)
         msg.channel.sendMessage(msg.content.substring(5, msg.content.length))
       })
-  } else if (msg.content === 'help') {
-    msg.channel.sendMessage('Don\'t be sad, you still have a zoidberg')
-  } else if (msg.content === 'how are you?') {
-    msg.channel.sendMessage('It\'s all so complicated with the flowers and the romance, and the lies upon lies!')
-  } else if (msg.content === 'where are you?') {
-    msg.channel.sendMessage('You’ll never guess where I’ve been!')
-  }
-})
-client.login(config.token)
+    } else if (msg.content === 'help') {
+      msg.channel.sendMessage('Don\'t be sad, you still have a zoidberg')
+    } else if (msg.content === 'how are you?') {
+      msg.channel.sendMessage('It\'s all so complicated with the flowers and the romance, and the lies upon lies!')
+    } else if (msg.content === 'where are you?') {
+      msg.channel.sendMessage('You’ll never guess where I’ve been!')
+    }
+  })
+  client.login(config.token)
