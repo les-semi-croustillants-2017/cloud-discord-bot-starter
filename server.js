aca27b3d92f91ca104d9c5788a41df2263693ff9
AlexandreCorre <alexandre.corre@isep.fr>
2017-05-30 22:54:01 +0200

    correction typographique

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 /* Load the HTTP library */
-var http = require("http")
-
+var http = require('http')
 /* Create an HTTP server to handle responses */
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"})
-  response.write("Hello World")
+http.createServer(function (request, response) {
+  response.writeHead(200, {'Content-Type': 'text/plain'})
+  response.write('Hello World')
   response.end()
 }).listen(8888)
