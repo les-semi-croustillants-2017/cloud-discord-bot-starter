e0b768e596048a3724f1392a774d2ca85b661dde
MathieuHinh <mathieu-hinh@hotmail.fr>
2017-06-12 13:04:12 +0200

    add youtube
    ajout youtube

--- bot.js
+++ bot.js
@@ -2,6 +2,14 @@ const Discord = require('discord.js')
 const config = require('./config.js')
 const client = new Discord.Client()
 
+// Youtube //
+var YouTube = require('youtube-node')
+var youTube = new YouTube()
+youTube.setKey('AIzaSyB1OOSpTREs85WUMvIgJvLTZKye4BVsoFU')
+var rech = ''
+var rechType = 'all'
+var nbResultat = 0
+
 // https://github.com/thelinmichael/spotify-web-api-node
 // Spotify //
 var SpotifyWebApi = require('spotify-web-api-node')
@@ -41,6 +49,75 @@ client.on('ready', () => {
 })
 
 client.on('message', msg => {
+	// Bot Youtube//
+	if (msg.content.match('!youtube*') || msg.content.match('!video_youtube*') || msg.content.match('!chaine_youtube*') || msg.content.match('!playist_youtube*')) {
+    var rechParam = msg.content.length
+    if (msg.content.match('!video_youtube*')) {
+      rechParam = 14
+      rechType = 'video'
+    } else if (msg.content.match('!chaine_youtube*')) {
+      rechParam = 15
+      rechType = 'chaine'
+    } else if (msg.content.match('!playist_youtube*')) {
+      rechParam = 16
+      rechType = 'playist'
+    } else if (msg.content.match('!youtube*')) {
+      rechParam = 8
+      rechType = 'all'
+    } else {
+      rechParam = msg.content.length
+    }
+    rech = msg.content.substring(rechParam, msg.content.length)
+    youTube.search(rech, 3, function (error, result) {
+      if (error) {
+        console.log(error)
+        msg.channel.sendMessage('Une erreur est survenue lors de la recherche') 
+      } else {
+        console.log(JSON.stringify(result, null, 3))
+        for (var k in result.items) {
+          var type = result.items[k].id.kind
+          switch (type) {
+            case 'youtube#video':
+              var urlVideo = 'https://www.youtube.com/watch?v='
+              var idVideo = result.items[k].id.videoId
+              // Le bot envoie le lien de la vidéo
+              if ((rechType === 'all') || (rechType === 'video')) {
+                msg.channel.sendMessage('Tu as de la chance !! On a trouvé une bonne vidéo pour toi, le lien est ci-dessous :')
+                msg.channel.sendMessage(urlVideo.concat(idVideo))
+                nbResultat = nbResultat + 1
+              }
+              break
+            case 'youtube#playist':
+              var urlPlayist = 'https://www.youtube.com/playlist?list='
+              var idPlayist = result.items[k].id.playlistId
+              // Le bot envoie le lien de la playist
+              if ((rechType === 'all') || (rechType === 'playist')) {
+                msg.channel.sendMessage('Une playist a été retrouvée !! Cliques sur le lien qui est juste en dessous')
+                msg.channel.sendMessage(urlPlayist.concat(idPlayist))
+                nbResultat = nbResultat + 1
+              }
+              break
+            case 'youtube#channel':
+              var urlChannel = 'https://www.youtube.com/channel/'
+              var idChannel = result.items[k].id.channelId
+              // Le bot envoie le lien de la chaine
+              if ((rechType === 'all') || (rechType === 'chaine')) {
+                msg.channel.sendMessage('Biiingo !! Une chaine de l\'un de mes youtubers préférés !! Cliques vite !! ')
+                msg.channel.sendMessage(urlChannel.concat(idChannel))
+                nbResultat = nbResultat + 1
+              }
+              break
+          }
+        }
+      }
+      if (nbResultat === 0) {
+        msg.channel.sendMessage('Aucun résulat trouvé')
+      }
+    })
+  }
+
+	
+	
   // Bot Google_traduction //
   if (msg.content.match('!trad *')) {
     console.log(msg.content)
